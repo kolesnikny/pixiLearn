f2c0adacbb63cc9a4b2a2431a5c3b3ec2d629fe0
Mykola Kolisnyk <kolesnikny@gmail.com>
2022-06-28 14:14:54 +0300

    Add poly

--- src/app.js
+++ src/app.js
@@ -10,11 +10,23 @@ app.renderer.backgroundColor = 0x23395d;
 
 app.renderer.resize(window.innerWidth, window.innerHeight);
 
-app.render.view.style.position = 'absolute';
+app.renderer.view.style.position = 'absolute';
 
 document.body.appendChild(app.view);
 
 const rectangle = new Graphics();
-rectangle.beginFill(0xaa33bb).drawRect(200, 200, 100, 120).endFill();
+rectangle
+  .beginFill(0xaa33bb)
+  .lineStyle(4, 0xffea00, 1)
+  .drawRect(200, 200, 100, 120)
+  .endFill();
 
 app.stage.addChild(rectangle);
+
+const poly = new Graphics();
+poly
+  .beginFill(0xff66ff)
+  .drawPolygon([600, 50, 800, 150, 900, 300, 400, 400])
+  .endFill();
+
+app.stage.addChild(poly);
